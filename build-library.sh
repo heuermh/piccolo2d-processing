363323013e82a1bb29840a23f4258fe8876e1305
Michael L Heuer <heuermh@acm.org>
2019-10-24 10:54:03 -0500

    update processing-core dependency version to 3.5.3

--- build-library.sh
+++ build-library.sh
@@ -10,6 +10,6 @@ cp lib/piccolo2d-core-3.0.1.jar piccolo2d/library
 cp -R src piccolo2d
 cp -R examples piccolo2d
 cd src
-javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.3.6.jar:../lib/piccolo2d-core-3.0.1.jar" org/piccolo2d/processing/Piccolo2D.java
+javac -source 1.8 -target 1.8 -classpath "../lib/processing-core-3.5.3.jar:../lib/piccolo2d-core-3.0.1.jar" org/piccolo2d/processing/Piccolo2D.java
 jar cvf ../piccolo2d/library/piccolo2d.jar org/piccolo2d/processing/Piccolo2D.class
 cd ..
